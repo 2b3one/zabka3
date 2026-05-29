19cfa822a74f4158e4c6d4944d44cbbe8a348f41
2b3one <pr.romaniuk@gmail.com>
2026-05-29 09:48:05 +0200

    Add files via upload

--- emulator/install_apk.sh
+++ emulator/install_apk.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+set -e
 
+# 1. Czekamy aż emulator się zgłosi do ADB
+echo "Czekam na emulator..."
+adb wait-for-device
+
+echo "Lista urządzeń:"
+adb devices
+
+# 2. Pobieramy APK
+echo "Pobieram APK Żappki..."
 curl -L -o zappka.apk "https://apkpure.com/pl/zappka/com.zabka.zappka/download"
 
-adb devices
+# 3. Instalacja
+echo "Instaluję APK..."
 adb install zappka.apk
